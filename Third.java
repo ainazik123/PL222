9e0c4b6f5c2f0e838a8de9bd61a33ca9d7536d7e
Ainazik <ainazik.zhanarbekova@alatoo.edu.kg>
2026-02-03 17:04:37 +0600

    add trird task

---
+++ Third.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> foods = new ArrayList<>();
+        foods.add("Burger");
+        foods.add("Pizza");
+        foods.add("Pasta");
+        foods.add("Pizza");
+        foods.add("Salad");
+        foods.add("Pizza");
+
+        int count = 0;
+
+        for (String food : foods) {
+            if (food.equals("Pizza")) {
+                count++;
+            }
+        }
+
+        System.out.println("Pizza appears " + count + " times");
+    }
+}
