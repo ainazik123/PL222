ea5d8352eab255295af04b6609faaacc35b72905
Ainazik <ainazik.zhanarbekova@alatoo.edu.kg>
2026-02-03 17:31:30 +0600

    add Tenth task

---
+++ Tenth.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Tenth {
+    public static void main(String[] args) {
+
+        Integer[] numbers = {1, 2, 3, 4, 5};
+
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(numbers));
+
+        System.out.println(arrayList);
+    }
+}
